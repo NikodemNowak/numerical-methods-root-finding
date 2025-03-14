a00d1d664c9b2c71583235d865960bdfbaa1c3fb
Mateusz Kosowski <mateuszkosowski03@gmail.com>
2025-03-14 17:10:25 +0100

    Rebuild bisekcja.py

--- bisekcja.py
+++ bisekcja.py
@@ -1,17 +1,21 @@
 from horner import horner
 
 def bisekcja(f, a, b, con, val, *args):
+
     c = 0
     licznik_przejsc = -1
 
     czy_wielomian = len(args) > 0
 
-    if czy_wielomian:
-        fa = horner(a, args[0], args[1])
-        fb = horner(b, args[0], args[1])
-    else:
-        fa = f(a)
-        fb = f(b)
+    def wartosc_funkcji_w_punkcie(x):
+        if czy_wielomian:
+            return horner(x, args[0], args[1])
+        else:
+            return f(x)
+
+    fa = wartosc_funkcji_w_punkcie(a)
+    fb = wartosc_funkcji_w_punkcie(b)
+
     if fa * fb > 0:
         raise ValueError("BISEKCJA: f(a) i f(b) muszą mieć przeciwne znaki")
 
@@ -20,25 +24,19 @@ def bisekcja(f, a, b, con, val, *args):
         for i in range(val):
             c = (a + b) / 2
 
-            if len(args) > 0:
-                fc = horner(c, args[0], args[1])
-            else:
-                fc = f(c)
+            fc = wartosc_funkcji_w_punkcie(c)
 
             if abs(fc) <= 0.0001:
                 return True, c, (i + 1)
 
-            if len(args) > 0:
-                fa = horner(a, args[0], args[1])
-                fc = horner(c, args[0], args[1])
-            else:
-                fa = f(a)
-                fc = f(c)
+            fa = wartosc_funkcji_w_punkcie(a)
+            fc = wartosc_funkcji_w_punkcie(c)
 
             if fa * fc < 0:
                 b = c
             else:
                 a = c
+
             licznik_przejsc = i
 
     # Dla dokładności
@@ -47,12 +45,8 @@ def bisekcja(f, a, b, con, val, *args):
             licznik_przejsc += 1
             c = (a + b) / 2
 
-            if len(args) > 0:
-                fa = horner(a, args[0], args[1])
-                fc = horner(c, args[0], args[1])
-            else:
-                fa = f(a)
-                fc = f(c)
+            fa= wartosc_funkcji_w_punkcie(a)
+            fc = wartosc_funkcji_w_punkcie(c)
 
             if abs(fc) < val:
                 return True, c, licznik_przejsc
